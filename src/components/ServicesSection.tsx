65fdd570d194d342c464848589b17cfb589269b0
mixxlizyn <yelizaveta_baturina@inbox.ru>
2025-10-17 16:50:24 +0500

    размер карточек

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -126,7 +126,7 @@ const ServicesSection = () => {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {services.map((service, index) => (
             <Card
               key={index}
